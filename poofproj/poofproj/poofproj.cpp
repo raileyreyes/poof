bd46198f47107318c8bf5381c30558dc35436532
raileyreyes <202410536@fit.edu.ph>
2024-11-22 09:27:06 +0800

    cin cout demos

---
+++ poofproj/poofproj/poofproj.cpp
@@ -0,0 +1,24 @@
+// poofproj.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Cin Cout
+    /*
+    int age;
+
+    cout << "Good Morning!\n";
+    cout << "Input your Age: ";
+        cin >> age;
+
+        cout << "You age is " << age << endl;
+
+        return 0;
+        */
+
+
+        
+}
